c273b934d10f5032c5b9395623b23545757e6479
EmilyVanVlerah <evanvlerah@fullsail.edu>
2014-03-04 09:33:53 -0500

    finished the first video

--- Week 1/Goal 1/goal1_basics/js/main.js
+++ Week 1/Goal 1/goal1_basics/js/main.js
@@ -59,14 +59,16 @@ console.log('------ variables ----------');
     //console.log ("name: ", name + " / course: ", course + " / month: ", month);
 
     //example 2: variables not defined will = "undefined"
-    var name, course, month;
-    console.log ("name: ", name + " / course: ", course + " / month: ", month);
+    //var name, course, month;
+    //console.log ("name: ", name + " / course: ", course + " / month: ", month);
 
     //example 3: a mix of variables defined and not defined
-
-
+    var  name, course = "PWA1", month;
+    console.log ("name: ", name + " / course: ", course + " / month: ", month);
+    
     //example 4: declaring multiple variables at once w/ same value
-
+    var a = b = c = d = 10;
+    console.log ("a: ", a + " / b: ", b + " / c: ", c + "/ d: ", d)
 
 
 // string (definition, concatenation, escape character)
