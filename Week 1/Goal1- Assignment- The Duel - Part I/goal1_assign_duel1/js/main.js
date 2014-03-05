6e39b770c68862eae86695625ec40092173d8969
EmilyVanVlerah <evanvlerah@fullsail.edu>
2014-03-05 11:00:18 -0500

    added console.log for troubleshooting

--- Week 1/Goal1- Assignment- The Duel - Part I/goal1_assign_duel1/js/main.js
+++ Week 1/Goal1- Assignment- The Duel - Part I/goal1_assign_duel1/js/main.js
@@ -27,13 +27,16 @@ Part 1 of 3
 		alert(playerOneName + ":" + playerOneHealth + "*START*" + playerTwoName + ":" + playerTwoHealth);
 
 		for (var i = 0; i < 10; i++) {
-			Math.floor(Math.random() * (max - min) + min);
+			//Math.floor(Math.random() * (max - min) + min);
 
 			var minDamage1 = player1Damage * .5;
 			var minDamage2 = player2Damage * .5;
 
-			var f1 = Math.floor(Math.random()*(player1Damage-minDamage1) + )
-			var f1 = Math.floor(Math.random()*(player2Damage-minDamage2) + )
+			var f1 = Math.floor(Math.random()*(player1Damage-minDamage1) + minDamage1);
+			var f1 = Math.floor(Math.random()*(player2Damage-minDamage2) + minDamage2);
+
+			console.log(f1);
+			console.log(f2);
 		};
 
 		winnerCheck();
