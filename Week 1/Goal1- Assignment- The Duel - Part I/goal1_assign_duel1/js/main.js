0a1288e66d70795556802dc4c18569d3fc099ad2
EmilyVanVlerah <evanvlerah@fullsail.edu>
2014-03-05 10:24:43 -0500

    Added player names, health, and damage.

--- Week 1/Goal1- Assignment- The Duel - Part I/goal1_assign_duel1/js/main.js
+++ Week 1/Goal1- Assignment- The Duel - Part I/goal1_assign_duel1/js/main.js
@@ -6,7 +6,20 @@ Assignment: Goal 1 - Duel Fight Game
 Part 1 of 3
 */
 
+(function () {
 
+	console.log("FIGHT!!!");
+
+	var playerOneName = "Spiderman";
+	var playerTwoName = "Batman";
+
+	var player1Damage = 20;
+	var player2Damage = 20;
+
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
+
+})();
 
 
 
