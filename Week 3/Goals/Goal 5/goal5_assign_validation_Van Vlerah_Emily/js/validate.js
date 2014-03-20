473fd82c45ff1f743158ba142e74c11a975c806a
EmilyVanVlerah <evanvlerah@fullsail.edu>
2014-03-20 15:28:40 -0400

    changed the java code in the pass variable

--- Week 3/Goals/Goal 5/goal5_assign_validation_Van Vlerah_Emily/js/validate.js
+++ Week 3/Goals/Goal 5/goal5_assign_validation_Van Vlerah_Emily/js/validate.js
@@ -44,7 +44,7 @@
 
         //};
 
-        var pass = validateField.test();
+        var pass = validateField();
         var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
 
         if (!pass || inputName.value.length < 2){
