472ee4a1850e7a219bba6852ffc6f22be42d24cc
EmilyVanVlerah <evanvlerah@fullsail.edu>
2014-03-20 15:40:40 -0400

    Changed the varabile that carries out through the code

--- Week 3/Goals/Goal 5/goal5_assign_validation_Van Vlerah_Emily/js/validate.js
+++ Week 3/Goals/Goal 5/goal5_assign_validation_Van Vlerah_Emily/js/validate.js
@@ -10,7 +10,7 @@
         //Below is one example of the validateField call with an argument.
         //You must dynamically retrieve the ID name from the DOM/HTML.
 
-        var info = document.querySelector('#f_username', '#f_email', '#f_phone', '#f_ssn', '#f_password');
+        var inputName = document.querySelector('#f_username', '#f_email', '#f_phone', '#f_ssn', '#f_password');
 
 
         validateField(id);  //id = is the form input field ID
@@ -21,7 +21,7 @@
     };
 
 
-    var validateField = function(info){
+    var validateField = function(inputName){
 
        // if (inputName.name === 'id name goes here'){
             //var pattern = 'a RegEx pattern goes here'
@@ -29,15 +29,15 @@
             //You will need to create an else-if statement for each input field id.  The
             //      format will be similar to the above IF statement.
 
-       if(info.name === 'f_username'){
+       if(inputName.name === 'f_username'){
             var pattern = 'a RegEx pattern goes here'
-       }else if(info.name === 'f_email'){
+       }else if(inputName.name === 'f_email'){
             var pattern = 'a RegEx pattern goes here'
-       }else if(info.name === 'f_phone'){
+       }else if(inputName.name === 'f_phone'){
             var pattern = 'a RegEx pattern goes here'
-       }else if(info.name === 'f_ssn'){
+       }else if(inputName.name === 'f_ssn'){
             var pattern = 'a RegEx pattern goes here'
-       }else if(info.name === 'f_password'){
+       }else if(inputName.name === 'f_password'){
             var pattern = 'a RegEx pattern goes here'
        }
 
