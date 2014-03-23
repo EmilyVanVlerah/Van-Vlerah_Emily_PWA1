7211118bbe549acea851456ec45d50c6dae3a15e
EmilyVanVlerah <evanvlerah@fullsail.edu>
2014-03-22 20:11:05 -0400

    Added new code to Mid term

--- Week 3/Goals/Goal 6/goal6_assign_midterm_Van Vlerah_Emily/js/main.js
+++ Week 3/Goals/Goal 6/goal6_assign_midterm_Van Vlerah_Emily/js/main.js
@@ -5,99 +5,142 @@
 
 	* Mid Terms for PWA-1
 */
-
-var students = [{name: 'jbond', address:{address: '3300 University Blvd', city:'Winter Park', state: 'FL'}, gpa: [2.5,3.5,4.0]},
-    {name: 'msanders', address:{address: '3322 University Street', city:'Orlando', state: 'FL'}, gpa: [2.3,3.7,4.0]}];
-
-var index = [0];
-
-
-var gpaAvg = function(students){
-
-    var total = 0;
-
-    for (var i = 0, j = students.gpa.length; i < j; i++){
-
-        total += students.gpa[i]["GPA"];
-    }
-
-    return total/students[i].gpa.length;
-
-};
-console.log("Average GPA:", gpaAvg(students));
+(function(){ // START - SELF-EXECUTING FUNCTION
 
 
 
-var date = new Date();
-
-console.log('-----------First Two Users-----------');
-
-for(var i = 0; i<students.length; i++){
-
-    console.log('Name: ',students[i].name);
-    console.log('Address: ',students[i].address['address'] + ', ',students[i].address['city'] + ', ',students[i].address['state']);
-    console.log('GPA: ',students[i].gpa.toString());
-    console.log('Date: ', date.toLocaleDateString());
-
- }
-
-//students.push({name: 'mmouse', address:{address: '1234 Sussex Ave', city:'Magic', state: 'MA'}, gpa: [2.0, 3.2, 4.0]});
-students.push({name: 'mmouse', address:{address: '1234 Sussex Ave', city:'Magic', state: 'MA'}, gpa: [2.0,3.2,4.0]});
-/*
-console.log('----------Three Users----------');
-
-function threeStudents(){
-
- for(var i = 0; i<students.length; i++){
-
-    console.log('Name: ',students[i].name);
-    console.log('Address: ',students[i].address['address'],students[i].address['city'],students[i].address['state'] );
-    console.log('GPA: ',students[i].gpa.toString());
-    console.log(date.toLocaleDateString());
-
- }
-    return threeStudents();
-}
-
-console.log(threeStudents());*/
+    var date = new Date();
+    var index = 0;
 
 
-function display(){
+    console.log('------ ORIGINAL Objects ----------');
 
-
-    if(index < students.length){
-
-        index++;
-
-        var innerName = document.getElementById("name");
-        var innerAddress = document.getElementById("address");
-        var innerGpa = document.getElementById("gpa");
-        var innerDate = document.getElementById('date');
-        var innerDate2 = date.toLocaleDateString();
-        //var innerAvg = document.getElementById('gpaavg');
-
-
-        //console.log(innerName);
-
-        innerName.innerHTML = 'Name: ' + students[index].name;
-        innerAddress.innerHTML = 'Address: ' + students[index].address.address + ", " + students[index].address.city + ", " + students[index].address.state;
-        innerGpa.innerHTML = 'GPA: ' + students[index].gpa;
-        innerDate.innerHTML = 'Date: ' + innerDate2;
-        //innerAvg.innerHTML = 'Average GPA: ';
-
-
-    }
-
-    return false;
-
-
-}
+    var students =  [
+        {
+            name: "James Bond",
+            address : {
+                address : "3300 University Blvd",
+                city    : "Winter Park",
+                state   : "Florida"
+            },
+            gpa : [2.5, 2.4, 3.3]
+        },
+        {
+            name: "Justin Goldman",
+            address : {
+                address : "4600 E Kentucky Ave",
+                city    : "Denver",
+                state   : "Colorado"
+            },
+            gpa : [2.4, 3.1, 3.4]
+        }];
 
 
 
-var button = document.getElementsByClassName('buttonred');
+    //CONSOLE OF ORIGINAL OBJECTS
+    for(var i = 0; i<students.length; i++){
+
+        console.log('Name: ',students[i].name);
+        console.log('Address: ',students[i].address['address'] + ', ',students[i].address['city'] + ', ',students[i].address['state']);
+        console.log('GPA: ',students[i].gpa.toString());
+        console.log('Date: ', date.toLocaleDateString());
+
+    };
 
 
-button[0].addEventListener('click', display, false);
+    //FUNCTION FOR NEW ARRAY
+    var addData = function(name, address, city, state, GPA){
 
-//document.querySelector('buttonred').innerHTML = 'DONE!!!';
+        var newStudent = {
+            name    : name,
+            address : {
+                address : address,
+                city    : city,
+                state   : state
+            },
+            gpa     : GPA
+        };
+
+
+        //PUSH NEW ARRAY
+        students.push(newStudent);
+    };
+
+    //CREATE DATA FOR NEW ARRAY
+
+    addData('super man', '123 Test Dr', 'Orlando', 'Florida', [3.2, 4.0, 2.2] );
+
+
+
+
+    console.log('------ ADDED Object ----------');
+
+
+    console.log("Students Name: ", students[2].name);
+    console.log("Students Address: ", students[2].address.address + "   " + students[2].address.city + ", "  + students[2].address.state);
+    console.log("Students GPA: ", students[2].gpa);
+    console.log("Students Date: ", date.toLocaleDateString());
+
+
+    //ONCLICK - EVENT
+
+
+    console.log(students);
+
+
+    document.getElementsByClassName('buttonred')[0].onclick = function(e){
+
+
+        display();
+
+
+        e.preventDefault();
+        return false;
+    };
+
+
+
+    function display(){
+
+
+        if(index < students.length){
+
+
+            var innerName = document.getElementById("name");
+            var innerAddress = document.getElementById("address");
+            var innerGpa = document.getElementById("gpa");
+            var innerDate = document.getElementById('date');
+            var innerDate2 = date.toLocaleDateString();
+            //var innerAvg = document.getElementById('gpaavg');
+
+
+            //console.log(innerName);
+
+            innerName.innerHTML = 'Name: ' + students[index].name;
+            innerAddress.innerHTML = 'Address: ' + students[index].address.address + ", " + students[index].address.city + ", " + students[index].address.state;
+            innerGpa.innerHTML = 'GPA: ' + students[index].gpa;
+            innerDate.innerHTML = 'Date: ' + innerDate2;
+            //innerAvg.innerHTML = 'Average GPA: ';
+            index++;
+
+
+        }else{
+
+            return false;
+        }
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+})(); //END - SELF EXECUTING FUNCTION
